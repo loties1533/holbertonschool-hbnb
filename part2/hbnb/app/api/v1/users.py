c2f6808bf538416acb1db6eb2770dd72be09d0f6
loties1533 <alexislaubert@yahoo.fr>
2026-02-24 18:54:09 +0000

    Nettoyage cache, transfert de main vers alexis et test T1

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -34,9 +34,18 @@ class UserList(Resource):
         if existing_user:
             return {'error': 'Email already registered'}, 400
 
-        new_user = facade.create_user(user_data)
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+        try:
+            new_user = facade.create_user(user_data)
+        except ValueError as e:
+            return {'error': str(e)}, 400
 
+        return {
+            'id': new_user.id,
+            'first_name': new_user.first_name,
+            'last_name': new_user.last_name,
+            'email': new_user.email
+        }, 201
+        
     @api.response(200, 'List of users retrieved successfully')
     def get(self):
         """Get list of all users"""
