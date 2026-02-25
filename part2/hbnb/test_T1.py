3baae8e99ec666da6b0e6c039876ee607dce4aa3
loties1533 <alexislaubert@yahoo.fr>
2026-02-25 12:10:57 +0000

    test_t1 valide passe toute les etapes

--- part2/hbnb/test_T1.py
+++ part2/hbnb/test_T1.py
@@ -4,10 +4,10 @@ from app.models.amenity import Amenity
 from app.models.review import Review
 
 def test_user_creation():
-    user = User(first_name="antoine", last_name="Doe", email="john.doe@example.com")
+    user = User(first_name="John", last_name="Doe", email="john.doe@example.com")
     assert user.first_name == "John"
     assert user.last_name == "Doe"
-    assert user.email == "john.doe@chocolatine.com"
+    assert user.email == "john.doe@example.com"
     assert user.is_admin is False  
     print("User creation test passed!")
 
