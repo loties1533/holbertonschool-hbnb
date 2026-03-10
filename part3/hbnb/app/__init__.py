e3924c0b5f22d5362b6b92dfc11ba2b8dab01df9
add1ktion <antoine.bats@gmail.com>
2026-03-10 11:54:45 +0100

    Added admin user

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,15 +4,10 @@ from flask_jwt_extended import JWTManager
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.users import api as users_ns
-from app.api.v1.auth import api as auth_ns
-
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
+
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -21,6 +16,12 @@ def create_app(config_class="config.DevelopmentConfig"):
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.auth import api as auth_ns
 
     api = Api(
         app, version='1.0',
