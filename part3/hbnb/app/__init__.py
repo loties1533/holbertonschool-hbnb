56eac80ddc6d2e7cf4971e4f85a71004a7071068
add1ktion <antoine.bats@gmail.com>
2026-03-10 10:07:18 +0100

    Implemented T5 SQLAlchemy Repository

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -9,6 +10,7 @@ from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.auth import api as auth_ns
 
+db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
 
@@ -16,6 +18,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
 
