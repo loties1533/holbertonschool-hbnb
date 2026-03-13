a82adf948e1957fa70cfea8d859700cbaec98260
loties <alexislaubert@yahoo.fr>
2026-03-13 10:55:31 +0100

    correction des noms des tables SQL qui correspond aux models SQLALCHEMY

--- part3/hbnb/sql/create_tables.sql
+++ part3/hbnb/sql/create_tables.sql
@@ -1,6 +1,6 @@
 -- User Table
 
-CREATE TABLE IF NOT EXISTS User (
+CREATE TABLE IF NOT EXISTS users (
     id CHAR(36) PRIMARY KEY,
     first_name  VARCHAR(255) NOT NULL,
     last_name   VARCHAR(255) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS User (
  
 -- Place Table
 
-CREATE TABLE IF NOT EXISTS Place (
+CREATE TABLE IF NOT EXISTS places (
     id CHAR(36) PRIMARY KEY,
     title  VARCHAR(255)  NOT NULL,
     description TEXT,
@@ -23,12 +23,12 @@ CREATE TABLE IF NOT EXISTS Place (
     owner_id CHAR(36) NOT NULL,
     created_at  DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  DATETIME  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (owner_id) REFERENCES User(id) ON DELETE CASCADE
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 );
  
 -- Review Table
 
-CREATE TABLE IF NOT EXISTS Review (
+CREATE TABLE IF NOT EXISTS reviews (
     id CHAR(36) PRIMARY KEY,
     text  TEXT NOT NULL,
     rating  INT NOT NULL CHECK (rating BETWEEN 1 AND 5),
@@ -36,14 +36,14 @@ CREATE TABLE IF NOT EXISTS Review (
     place_id  CHAR(36) NOT NULL,
     created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id)  REFERENCES User(id)  ON DELETE CASCADE,
-    FOREIGN KEY (place_id) REFERENCES Place(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id)  REFERENCES users(id)  ON DELETE CASCADE,
+    FOREIGN KEY (place_id) REFERENCES places(id) ON DELETE CASCADE,
     UNIQUE (user_id, place_id)  -- un seul avis par utilisateur et par logement
 );
  
 -- Amenity Table
 
-CREATE TABLE IF NOT EXISTS Amenity (
+CREATE TABLE IF NOT EXISTS amenities (
     id CHAR(36) PRIMARY KEY,
     name  VARCHAR(255) NOT NULL UNIQUE,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -53,10 +53,10 @@ CREATE TABLE IF NOT EXISTS Amenity (
 -- Place_Amenity Table (Many-to-Many)
 -- table d'association entre Place et Amenity ,avec des clés étrangères vers les deux tables et une clé primaire
 
-CREATE TABLE IF NOT EXISTS Place_Amenity (
+CREATE TABLE IF NOT EXISTS place_amenity (
     place_id    CHAR(36) NOT NULL,
     amenity_id  CHAR(36) NOT NULL,
     PRIMARY KEY (place_id, amenity_id),
-    FOREIGN KEY (place_id) REFERENCES Place(id)   ON DELETE CASCADE,
-    FOREIGN KEY (amenity_id) REFERENCES Amenity(id) ON DELETE CASCADE
+    FOREIGN KEY (place_id) REFERENCES places(id)   ON DELETE CASCADE,
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id) ON DELETE CASCADE
 );
